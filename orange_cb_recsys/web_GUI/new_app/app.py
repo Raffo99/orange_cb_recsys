3a0d32541b9a598cce924491d326e03d386b7203
Raffo99 <dis.raffaele99@gmail.com>
2021-07-18 15:21:53 +0200

    Aggiornamento per includere la classe project

--- orange_cb_recsys/web_GUI/new_app/app.py
+++ orange_cb_recsys/web_GUI/new_app/app.py
@@ -13,15 +13,14 @@ from utils.algorithms import get_ca_algorithms
 from utils.algorithms import get_recsys_algorithms
 from utils.forms import allowed_file
 
+from project import Project, ContentAnalyzerModule, PossiblePageStatus
+
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['UPLOAD_FOLDER'] = join(dirname(realpath(__file__)), 'uploads\\')
 
 # Global vars for content_analyzer
-fields = {}
-content_production_algorithms = []
-preprocessing_algorithms = []
-memory_interfaces = []
+current_project = Project()
 
 # Global vars for recsys
 recsys_content = None
@@ -29,15 +28,14 @@ recsys_content = None
 
 @app.route('/')
 def index():
-    c = get_recsys_algorithms()
-    c, p, t = get_ca_algorithms()
-    print(c)
     return render_template("index.html")
 
 
 # Pagina per caricare il dataset (CONTENT ANALYZER)
 @app.route('/content-analyzer/upload', methods=['POST', 'GET'])
 def ca_upload():
+    global current_project
+
     if request.method == 'POST':
         file = request.files['pathDataset']
         if file.filename == '':
@@ -47,72 +45,76 @@ def ca_upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            session['dirOutput'] = request.form['outputDir']
-            session['pathDataset'] = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            fields.clear()
-            return redirect(url_for('ca_fields'))
 
-    return render_template('/content-analyzer/upload.html')
+            current_project.content_analyzer.set_output_directory(request.form["outputDir"])
+            current_project.content_analyzer.set_source_path(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            current_project.content_analyzer.clear_fields()
+            current_project.content_analyzer.clear_id_fields()
+
+            current_project.content_analyzer.set_page_status("Upload", PossiblePageStatus.COMPLETE)
+            current_project.content_analyzer.set_page_status("Fields", PossiblePageStatus.INCOMPLETE)
+            current_project.content_analyzer.set_page_status("Algorithms", PossiblePageStatus.DISABLED)
+            current_project.content_analyzer.set_page_status("Execute", PossiblePageStatus.DISABLED)
+            return redirect(url_for('ca_fields'))
+    return render_template('/content-analyzer/upload.html', project=current_project)
 
 
 # Pagina per scegliere quali campi utilizzare del dataset (CONTENT ANALYZER)
 @app.route('/content-analyzer/fields', methods=['POST', 'GET'])
 def ca_fields():
-    global fields
+    global current_project
 
     if request.method == "POST":
         new_fields = dict(request.form.items(multi=False))
-
-        for key in list(fields):
-            if not(key in new_fields):
-                fields.pop(key)
+        for key in list(current_project.content_analyzer.get_fields()):
+            if not(key in list(map(lambda x: ContentAnalyzerModule.convert_key(x), new_fields))):
+                current_project.content_analyzer.pop_field(key)
 
         for key, value in new_fields.items():
-            if key == "id_dataset":
-                session['id_dataset'] = value
-            elif value == "on":
-                if not(key in fields):
-                    fields[key] = []
+            if "__fieldid" in key:
+                current_project.content_analyzer.add_id_field(key.replace("__fieldid", ""))
+            elif not (ContentAnalyzerModule.convert_key(key) in current_project.content_analyzer.get_fields()):
+                current_project.content_analyzer.set_field(key, [])
 
-        # TODO: Ottimizare questa parte di codice per ordinare i campi
-        temp_fields = fields.copy()
-        fields.clear()
-        for key in list(new_fields):
-            if new_fields[key] == "on":
-                fields[key] = temp_fields[key]
-
-        return redirect(url_for('ca_settings'))
+        if not current_project.content_analyzer.get_fields():
+            # TODO: Dare errore nel caso in cui fields è vuoto
+            return "ERRORE"
+        else:
+            current_project.content_analyzer.order_fields()
+            current_project.content_analyzer.set_page_status("Fields", PossiblePageStatus.COMPLETE)
+            current_project.content_analyzer.set_page_status("Algorithms", PossiblePageStatus.INCOMPLETE)
+            return redirect(url_for('ca_settings'))
 
     try:
-        df = pd.read_csv(session['pathDataset'])
+        # TODO: Supportare anche json e dat
+        df = pd.read_csv(current_project.content_analyzer.get_source_path())
     except:
-        # TODO: Creare pagina di errore nel caso in cui pathDataset non sia settato
+        # TODO: Dare errore nel caso in cui pathDataset non sia settato
         return "Errore"
 
-    return render_template('/content-analyzer/fields.html', fields=df.columns.values)
+    return render_template('/content-analyzer/fields.html', fields=df.columns.values, project=current_project)
 
 
 # Pagina per le varie impostazioni dei campi del dataset scelti precedentemente (CONTENT ANALYZER)
-@app.route('/content-analyzer/settings', methods=['POST', 'GET'])
+@app.route('/content-analyzer/algorithms', methods=['POST', 'GET'])
 def ca_settings():
-    global fields
-    global content_production_algorithms
-    global preprocessing_algorithms
-    global memory_interfaces
+    global current_project
 
     content_production_algorithms, preprocessing_algorithms, memory_interfaces = get_ca_algorithms()
-    print(content_production_algorithms)
+    current_project.content_analyzer.set_content_production_algorithms(content_production_algorithms)
+    current_project.content_analyzer.set_preprocess_algorithms(preprocessing_algorithms)
+    current_project.content_analyzer.set_memory_interfaces(memory_interfaces)
 
-    return render_template('/content-analyzer/settings.html', fields=fields, cp_algorithms=content_production_algorithms)
+    return render_template('/content-analyzer/settings.html', fields=current_project.content_analyzer.get_fields(),
+                           cp_algorithms=content_production_algorithms, project=current_project)
 
 
 # Pagina di supporto per la creazione dei form dinamici della pagina ca-settings
 @app.route('/_representationformcreator', methods=['POST'])
 def representation_form_creator():
-    global fields
-    global content_production_algorithms
-    global preprocessing_algorithms
-    global memory_interfaces
+    global current_project
+    fields = current_project.content_analyzer.get_fields()
 
     has_representation = request.json['has_representation']
 
@@ -121,12 +123,13 @@ def representation_form_creator():
         representations = fields[field_name]
 
     else:
+        content_production_algorithms = current_project.content_analyzer.get_content_production_algorithms()
         algorithm = [a for a in content_production_algorithms if a["name"] == request.json['algorithm_name']][0]
 
         representations = [{
             'id': 'default',
             'algorithm': algorithm,
-            'preprocess': preprocessing_algorithms,
+            'preprocess': current_project.content_analyzer.get_preprocess_algorithms(),
         }]
 
     return render_template("/content-analyzer/helpers/_representationformcreator.html", representations=representations)
@@ -135,12 +138,11 @@ def representation_form_creator():
 # Pagina di supporto per aggiornare le rappresentazioni dei campi
 @app.route('/ca-update-representations', methods=['POST', 'GET'])
 def ca_update_representations():
-    global fields
+    global current_project
 
     if request.form:
         if not("delete_representation" in request.form):
             delete_representation = False
-            print(request.form['representations'])
             new_representations = json.loads(request.form['representations'])
         else:
             delete_representation = True
@@ -149,7 +151,6 @@ def ca_update_representations():
     else:
         if not("delete_representation" in request.json):
             delete_representation = False
-            print(request.json['representations'])
             new_representations = request.json['representations']
         else:
             delete_representation = True
@@ -157,11 +158,11 @@ def ca_update_representations():
         field_name = request.json['field_name']
 
     if delete_representation:
-        fields[field_name].pop(index_representation)
+        print(field_name)
+        print(index_representation)
+        current_project.content_analyzer.pop_representation(field_name, index_representation)
     else:
-        fields[field_name] = new_representations
-
-    print(fields)
+        current_project.content_analyzer.set_field(field_name, new_representations)
 
     return ""
 
@@ -169,6 +170,7 @@ def ca_update_representations():
 @app.route("/recsys/upload", methods=['POST', 'GET'])
 def recsys_upload():
     global recsys_content
+    global current_project
 
     list_errors = []
     if request.method == 'POST':
@@ -196,16 +198,18 @@ def recsys_upload():
         except OSError:
             list_errors.append("Wrong syntax in path to content.")
 
-    return render_template("./recsys/upload.html", list_errors=list_errors)
+    return render_template("./recsys/upload.html", list_errors=list_errors, project=current_project)
 
 
 @app.route("/recsys/representations", methods=['POST', 'GET'])
 def recsys_representations():
     global recsys_content
+    global current_project
 
-    # algorithms = get_recsys_algorithms()
+    algorithms = get_recsys_algorithms()
 
-    return render_template("/recsys/representations.html", fields_representations=recsys_content.field_dict)
+    return render_template("/recsys/representations.html", fields_representations=recsys_content.field_dict,
+                           algorithms=algorithms, project=current_project)
 
 
 if __name__ == '__main__':
