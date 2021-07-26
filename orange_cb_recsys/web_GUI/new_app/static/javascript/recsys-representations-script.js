9823829b2b7ef25b0c2503f2d96e38add5d0a49d
Raffo99 <dis.raffaele99@gmail.com>
2021-07-26 12:58:47 +0200

    Aggiunta funzione di salvataggio

--- orange_cb_recsys/web_GUI/new_app/static/javascript/recsys-representations-script.js
+++ orange_cb_recsys/web_GUI/new_app/static/javascript/recsys-representations-script.js
@@ -1,9 +1,72 @@
-import { changeActiveBlock } from "./utils-functions.js";
+import { changeActiveBlock, getClassWithParameters } from "./utils-functions.js";
+
+function saveAlgorithms() {
+    let listAlgorithms = []
+
+    $(".block-algorithm").each(function () {
+        let listParameters = []
+
+        $(this).children(".block-parameter").each(function () {
+            listParameters.push(getClassWithParameters($(this)));
+        });
+
+        listAlgorithms.push({
+            "name": $(this).attr('name').replace("algtype-", ""),
+            "params": listParameters
+        });
+    })
+
+    let selectedAlgorithm = $(".select-algorithm").val();
+
+    let listFields = []
+    if (selectedAlgorithm == "ContentBasedRS") {
+        $(".field-wrapper").each(function () {
+            let fieldRepresentations = []
+            let fieldName = $(this).children(".field-name").text();
+
+            $(this).children(".representations-list").children(".representation-item").each(function () {
+               let representationName = $(this).children(".representation-label").text().trim();
+
+               fieldRepresentations.push({
+                   "name": representationName,
+                   "use": $(this).children(".representation-label").children("input").prop("checked")
+               });
+            });
+
+            listFields.push({
+                "name": fieldName,
+                "use": $(this).children(".field-name").children("input").prop("checked"),
+                "representations": fieldRepresentations
+            })
+        });
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: "/update-recsys-algorithm",
+        contentType: 'application/json; charset=UTF-8',
+        data: JSON.stringify({
+            "algorithms": listAlgorithms,
+            "selectedAlgorithm": selectedAlgorithm,
+            "listFields": listFields
+        })
+    });
+}
 
 $(document).ready(function () {
+    $("#continue-button").click(function () {
+       saveAlgorithms();
+       // Redirect to next section
+    });
+
+    $("#save-form").click(function () {
+        saveAlgorithms();
+    });
+
     if ($(".select-algorithm").val() == "GraphBasedRS") {
         $(".header-representations").css("display", "none");
         $(".div-content-representations").css("display", "none");
+        $(".col-6").removeClass("col-6").addClass("col-8");
     }
 
     $("select").each(function () {
@@ -13,12 +76,12 @@ $(document).ready(function () {
     $("select").change(function () {
         if ($(this).val() == "ContentBasedRS") {
             $(".col-8").removeClass("col-8").addClass("col-6");
-            $(".header-representations").slideToggle(function () {
-                $(".div-content-representations").slideToggle();
+            $(".header-representations").stop().slideToggle(function () {
+                $(".div-content-representations").stop().slideToggle();
             });
         } else if ($(this).val() == "GraphBasedRS") {
-            $(".div-content-representations").slideToggle(function () {
-                $(".header-representations").slideToggle(function () {
+            $(".div-content-representations").stop().slideToggle(function () {
+                $(".header-representations").stop().slideToggle(function () {
                       $(".col-6").removeClass("col-6").addClass("col-8");
                 });
             });
