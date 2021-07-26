b0beafc2135ecd2de37947a38a83acfd4931b4c1
Raffo99 <dis.raffaele99@gmail.com>
2021-07-26 12:48:00 +0200

    Spostata la funzione per creare JSON di classi con rispettivi parametri nel file utils-functions.js

--- orange_cb_recsys/web_GUI/new_app/static/javascript/utils-functions.js
+++ orange_cb_recsys/web_GUI/new_app/static/javascript/utils-functions.js
@@ -1,3 +1,76 @@
+// Support method used to generate the JSON of the classes of the current active field representations
+export function getClassWithParameters(blockParameter) {
+    const types = {
+        "text": "str",
+        "number": "int",
+        "checkbox": "bool"
+    }
+
+    if (blockParameter.children("[class='block-union-selection']").length > 0) {
+        // Parameter is Union
+        let parameters = [];
+        blockParameter.children("[class='block-union-parameter']").children("[class*='block-parameter']").each(function () {
+            parameters.push(getClassWithParameters($(this)));
+        });
+
+        blockParameter.children("[class='block-union-selection']").children("select").children("option").each(function (index) {
+            parameters[index]["name"] = $(this).val();
+        });
+
+        return {
+            'name': blockParameter.children("[class='block-union-selection']").children("label").text(),
+            'type': 'Union',
+            'value': blockParameter.children("[class='block-union-selection']").children("select").val(),
+            'params': parameters
+        }
+    } else {
+        let blockParameterContainer = (blockParameter.children("[class*='block-parameter-container']"));
+
+        if (blockParameterContainer.children("[class*='block-sub-classes']").length > 0) {
+            // Parameter is Complex with sub classes
+            let subClasses = [];
+            let blockSubClasses = blockParameterContainer.children("[class='block-sub-classes']");
+            blockSubClasses.children("[class*='block-parameter']").each(function () {
+                subClasses.push(getClassWithParameters($(this)));
+            });
+
+            blockParameterContainer.children("[class='block-sub-classes-selection']").children("select").children("option").each(function (index) {
+               subClasses[index]["name"] = $(this).val();
+            });
+
+            return {
+                'name': blockParameterContainer.children("[class='block-sub-classes-selection']").children("label").text(),
+                'type': 'Complex',
+                'value': blockParameterContainer.children("[class='block-sub-classes-selection']").children("select").val(),
+                'sub_classes': subClasses
+            }
+        } else if (blockParameterContainer.children("[class='block-parameter']").length > 0
+                        || blockParameterContainer.length == 0) {
+            // Parameter is Complex with parameters
+            let parameters = [];
+            blockParameterContainer.children("[class*='block-parameter']").each(function () {
+                parameters.push(getClassWithParameters($(this)));
+            });
+
+            return {
+                'name': blockParameterContainer.children("label").text(),
+                'type': 'Complex',
+                'params': parameters
+            }
+        } else {
+            let value = blockParameterContainer.children("input").attr("type") == "checkbox" ?
+                blockParameterContainer.children("input").is(":checked") :
+                blockParameterContainer.children("input").val();
+            // Parameter is Simple
+            return {
+                'name': blockParameterContainer.children("label").text(),
+                'type': types[blockParameterContainer.children("input").attr('type')],
+                'value': value
+            }
+        }
+    }
+}
+
 export function changeActiveBlock(selectObj) {
     let nameToActive = selectObj.find('option:selected').attr('value').replace(" ", "");
     let nameParam = selectObj.attr('name');
@@ -9,15 +82,27 @@ export function changeActiveBlock(selectObj) {
     toRemove.css("display", "");
     toRemove.removeClass("active-block");
     toAdd.addClass("active-block");
-    toAdd.fadeIn();
-    toAdd.css("display", "block");
 
-    toAdd.find("select").each(function () {
-        changeActiveBlock($(this));
-    });
+    if (!toAdd.hasClass("empty")) {
+        toAdd.fadeIn();
+        toAdd.css("display", "block");
+        toAdd.find("select").each(function () {
+            changeActiveBlock($(this));
+        });
+    }
+
 }
 
 export function fixName(name, isAlgorithmName) {
     return (isAlgorithmName ? name.trim().replace("\n", "").replace(" ", "_") :
         name.trim().replace("\n", "").replace(" ", "_").toLowerCase());
+}
+
+export function showToast(message, timeDisplay) {
+    $("#toast").children("label").text(message);
+    $("#toast").fadeIn();
+
+    setTimeout(function () {
+        $("#toast").fadeOut();
+    }, timeDisplay);
 }
