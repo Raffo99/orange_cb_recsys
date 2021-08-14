c660429c7c7ad2f4a00c21a54e54deef076b36ff
Raffo99 <dis.raffaele99@gmail.com>
2021-08-14 11:57:50 +0200

    Exogenous + fix alle altre pagine

--- orange_cb_recsys/web_GUI/new_app/app.py
+++ orange_cb_recsys/web_GUI/new_app/app.py
@@ -38,8 +38,12 @@ def all_test(dictionary):
 app.jinja_env.globals.update(all_python=all_test)
 
 # Global variable for the current project
-dbpedia_classes = get_dbpedia_classes()
-current_project = Project(get_recsys_algorithms(), dbpedia_classes)
+try:
+    dbpedia_classes = get_dbpedia_classes()
+except Exception:
+    print("Error while loading dbpedia classes, these won't be used")
+    dbpedia_classes = []
+current_project = Project(get_ca_algorithms(), get_recsys_algorithms(), dbpedia_classes)
 
 
 @app.route('/', methods=["POST", "GET"])
@@ -59,7 +63,7 @@ def index():
             else:
                 list_errors.append("Save path is invalid!")
         else:
-            current_project = Project(get_recsys_algorithms(), dbpedia_classes)
+            current_project = Project(get_ca_algorithms(), get_recsys_algorithms(), dbpedia_classes)
             current_project.content_analyzer_items.set_page_status("Upload", PossiblePageStatus.INCOMPLETE)
             current_project.content_analyzer_users.set_page_status("Upload", PossiblePageStatus.INCOMPLETE)
             current_project.content_analyzer_ratings.set_page_status("Upload", PossiblePageStatus.INCOMPLETE)
@@ -105,8 +109,9 @@ def ca_upload():
                 data = json.load(open(content_analyzer.source_path))
                 content_analyzer.fields_list = list(data[0].keys())
 
-            content_analyzer.clear_fields()
-            content_analyzer.clear_id_fields()
+            if content_analyzer_type != "Ratings":
+                content_analyzer.clear_fields()
+                content_analyzer.clear_id_fields()
 
             content_analyzer.set_page_status("Upload", PossiblePageStatus.COMPLETE)
             content_analyzer.set_page_status("Fields", PossiblePageStatus.INCOMPLETE)
@@ -137,30 +142,43 @@ def ca_fields():
     content_analyzer = current_project.content_analyzer[content_analyzer_type]
 
     if request.method == "POST":
-        new_fields = dict(request.form.items(multi=False))
-        for key in list(content_analyzer.fields_selected):
-            if not(key in list(map(lambda x: ContentAnalyzerModule.convert_key(x), new_fields))):
-                content_analyzer.pop_field(key)
+        # TODO: Ratings in ca
+        if request.args.get("type") == "Ratings":
+            from_id_column = request.form["fromIdColumn"]
+            to_id_column = request.form["toIdColumn"]
+            score_column = request.form["scoreColumn"]
+            timestamp_column = request.form["timestampColumn"]
 
-        temp_fields_id = []
-        for key, value in new_fields.items():
-            if "__fieldid" in key:
-                temp_fields_id.append(key.replace("__fieldid", ""))
-            elif not (ContentAnalyzerModule.convert_key(key) in content_analyzer.fields_selected):
-                content_analyzer.set_field(key, [])
-
-        content_analyzer.id_fields_name = temp_fields_id
-
-        if not content_analyzer.fields_selected:
-            # TODO: Dare errore nel caso in cui fields è vuoto
-            return "ERRORE"
+            if [from_id_column, to_id_column, score_column] in [content_analyzer.fields_list] and \
+                    (timestamp_column in [content_analyzer.fields_list, None]):
+                content_analyzer.from_id_column = from_id_column
+                content_analyzer.to_id_column = to_id_column
+                content_analyzer.score_column = score_column
+                content_analyzer.timestamp_column = timestamp_column
         else:
+            new_fields = dict(request.form.items(multi=False))
+            for key in list(content_analyzer.fields_selected):
+                if not(key in list(map(lambda x: ContentAnalyzerModule.convert_key(x), new_fields))):
+                    content_analyzer.pop_field(key)
+
+            temp_fields_id = []
+            for key, value in new_fields.items():
+                if "__fieldid" in key:
+                    temp_fields_id.append(key.replace("__fieldid", ""))
+                elif not (ContentAnalyzerModule.convert_key(key) in content_analyzer.fields_selected):
+                    content_analyzer.set_field(key, [])
+
+            content_analyzer.id_fields_name = temp_fields_id
             content_analyzer.order_fields()
 
+        if content_analyzer_type == "Ratings" or content_analyzer.fields_selected:
             content_analyzer.set_page_status("Fields", PossiblePageStatus.COMPLETE)
             content_analyzer.set_page_status("Algorithms", PossiblePageStatus.INCOMPLETE)
             content_analyzer.set_page_status("Exogenous", PossiblePageStatus.INCOMPLETE)
             return redirect(url_for('ca_settings') + "?type=" + content_analyzer_type)
+        else:
+            # TODO: Dare errore nel caso in cui fields è vuoto
+            return "ERRORE"
 
     return render_template('/content-analyzer/fields.html', fields=content_analyzer.fields_list,
                            project=current_project, content_analyzer=content_analyzer,
@@ -184,12 +202,13 @@ def ca_settings():
 
     content_analyzer = current_project.content_analyzer[content_analyzer_type]
 
-    # TODO: This code needs to be execute in the class, not here
-    content_analyzer.content_production_algorithms, \
-        content_analyzer.preprocess_algorithms, \
-        content_analyzer.memory_interfaces,\
-        content_analyzer.exogenous_algorithms,\
-        content_analyzer.ratings_algorithms = get_ca_algorithms()
+    dict_algorithms = get_ca_algorithms()
+
+    content_analyzer.content_production_algorithms = dict_algorithms["content_production"]
+    content_analyzer.preprocess_algorithms = dict_algorithms["preprocessing"]
+    content_analyzer.memory_interfaces = dict_algorithms["memory_interfaces"]
+    content_analyzer.exogenous_algorithms = dict_algorithms["exogenous"]
+    content_analyzer.ratings_algorithms = dict_algorithms["ratings"]
 
     return render_template('/content-analyzer/settings.html', fields=content_analyzer.fields_selected,
                            project=current_project, content_analyzer=content_analyzer,
@@ -207,8 +226,61 @@ def ca_exogenous():
             or current_project.content_analyzer[content_analyzer_type].get_page_status("Exogenous") == PossiblePageStatus.DISABLED:
         return redirect(url_for("index"))
 
+    content_analyzer = current_project.content_analyzer[content_analyzer_type]
+
     return render_template('content-analyzer/exogenous.html', project=current_project,
-                           content_type=content_analyzer_type)
+                           content_type=content_analyzer_type, exogenous=content_analyzer.exogenous_techniques,
+                           fields_list=content_analyzer.fields_list, entity_list=dbpedia_classes)
+
+
+@app.route('/ca-update-exogenous', methods=['POST'])
+def ca_update_exogenous():
+    global current_project
+
+    if current_project.is_first_project():
+        return redirect(url_for("index"))
+
+    if request.form:
+        action = request.form["action"]
+        content_type = request.form["contentType"]
+        content_analyzer = current_project.content_analyzer[content_type]
+
+        if action == "remove":
+            technique_index = int(request.form["techniqueIndex"])
+            content_analyzer.remove_exogenous_technique(technique_index)
+        elif action == "update":
+            technique_content = json.loads(request.form["techniqueContent"])
+            technique_index = int(request.form["techniqueIndex"])
+            content_analyzer.update_exogenous_technique(technique_index, technique_content)
+
+        if len(content_analyzer.exogenous_techniques) > 0:
+            content_analyzer.set_page_status("Exogenous", PossiblePageStatus.COMPLETE)
+        else:
+            content_analyzer.set_page_status("Exogenous", PossiblePageStatus.INCOMPLETE)
+
+    print(content_analyzer.exogenous_techniques)
+
+    return {"result": True}
+
+
+@app.route('/_exogenousformcreator', methods=['POST'])
+def exogenous_form_creator():
+    global current_project
+
+    content_analyzer = current_project.content_analyzer[request.json["content_type"]]
+    technique_index = request.json["technique_index"]
+
+    if technique_index == -1:
+        technique = {
+            "content": content_analyzer.exogenous_algorithms,
+            "fields_list": [{"name": field, "value": False} for field in content_analyzer.fields_list]
+        }
+        technique["content"][0]["params"][1]["value"] = "Default"
+        content_analyzer.add_exogenous_technique(technique)
+    else:
+        technique = content_analyzer.exogenous_techniques[technique_index]
+
+    return render_template("/content-analyzer/helpers/_exogenousformcreator.html", technique=technique)
 
 
 @app.route('/_representationformcreator', methods=['POST'])
@@ -257,7 +329,8 @@ def representation_form_creator():
                 'memory_interfaces': memory_interfaces
             }]
 
-    return render_template("/content-analyzer/helpers/_representationformcreator.html", representations=representations)
+    return render_template("/content-analyzer/helpers/_representationformcreator.html",
+                           representations=representations, fields_list=content_analyzer.fields_list)
 
 
 @app.route('/ca-update-representations', methods=['POST', 'GET'])
@@ -278,6 +351,8 @@ def ca_update_representations():
 
     if current_project.is_first_project():
         return redirect(url_for("index"))
+
+    # TODO: use only .form and not .json
 
     if request.form:
         if not("delete_representation" in request.form):
@@ -322,22 +397,37 @@ def recsys_upload():
 
     list_errors = []
     if request.method == 'POST':
-        items_path = request.form["pathItems"]
-        users_path = request.form["pathUsers"]
+        items_ca = request.form["useContentItems"] if "useContentItems" in request.form else False
+        current_project.recommender_system.set_path_from_ca("Items", items_ca)
+        users_ca = request.form["useContentUsers"] if "useContentUsers" in request.form else False
+        current_project.recommender_system.set_path_from_ca("Users", users_ca)
+        ratings_ca = request.form["useContentRatings"] if "useContentRatings" in request.form else False
+        current_project.recommender_system.set_path_from_ca("Ratings", ratings_ca)
+
+        items_path = request.form["pathItems"] if "pathItems" in request.form else ""
+        users_path = request.form["pathUsers"] if "pathUsers" in request.form else ""
+        ratings_path = request.form["pathRatings"] if "pathRatings" in request.form else ""
+
         output_directory = request.form["outputDir"]
-        ratings_path = request.form["pathRatings"]
+
         try:
-            if items_path == "" and current_project.recommender_system.items_path != "" \
+            # TODO: Users is not used in some algs, so no checks in future
+            # Various checks fro the paths of items, users and ratings
+            if not items_ca and items_path == "" and current_project.recommender_system.items_path != "" \
                     and not is_pathname_valid(items_path):
                 list_errors.append("Path to items is invalid.")
             else:
                 current_project.recommender_system.items_path = items_path
-
-            if users_path == "" and current_project.recommender_system.users_path != "" \
+            if not users_ca and users_path == "" and current_project.recommender_system.users_path != "" \
                     and not is_pathname_valid(users_path):
                 list_errors.append("Path to users is invalid.")
             else:
                 current_project.recommender_system.users_path = users_path
+            if not ratings_ca and ratings_path == "" and current_project.recommender_system.ratings_path != ""\
+                    and not is_pathname_valid(ratings_path):
+                list_errors.append("Path to ratings is invalid.")
+            else:
+                current_project.recommender_system.ratings_path = ratings_path
 
             if output_directory == "" and current_project.recommender_system.output_directory != "" \
                     and not is_pathname_valid(output_directory):
@@ -345,31 +435,65 @@ def recsys_upload():
             else:
                 current_project.recommender_system.output_directory = output_directory
 
-            if ratings_path == "" and current_project.recommender_system.ratings_path != ""\
-                    and not is_pathname_valid(ratings_path):
-                list_errors.append("Path to ratings is invalid.")
-            else:
-                current_project.recommender_system.ratings_path = ratings_path
-
             if len(list_errors) == 0:
-                list_files = [f for f in os.listdir(items_path) if os.path.isfile(join(items_path, f)) and ".xz" in f]
-                file_to_check = list_files[0]
-                content = load_content_instance(items_path, file_to_check.replace(".xz", ""))
-                if isinstance(content, Content):
-                    current_project.recommender_system.content = content
+                if not items_ca:
+                    list_errors = list_errors + check_contents(items_path, "Items")
+                if not users_ca:
+                    list_errors = list_errors + check_contents(users_path, "Users")
+                # if not ratings_ca:
+                #     list_errors.append(check_contents(ratings_path, "Ratings"))
+
+                if len(list_errors) == 0:
                     current_project.recommender_system.set_page_status("Upload", PossiblePageStatus.COMPLETE)
                     current_project.recommender_system.set_page_status("Representations", PossiblePageStatus.INCOMPLETE)
                     return redirect(url_for('recsys_representations'))
-                else:
-                    list_errors.append("Invalid items file in items directory <br>(<b>'" + items_path + "'</b>)")
         except IndexError:
             list_errors.append("There is no valid file in the items directory <br>(<b>'" + items_path + "'</b>)")
-        except FileNotFoundError:
-            list_errors.append("<b>'" + items_path + "'</b> is not a valid directory.")
-        except OSError:
-            list_errors.append("Wrong syntax in path to content.")
-
+    print(list_errors)
     return render_template("./recsys/upload.html", list_errors=list_errors, project=current_project)
+
+
+def check_contents(path_to_content, content_type):
+    global current_project
+
+    list_errors = []
+    try:
+        list_files = [f for f in os.listdir(path_to_content) if os.path.isfile(join(path_to_content, f)) and ".xz" in f]
+        file_to_check = list_files[0]
+        content = load_content_instance(path_to_content, file_to_check.replace(".xz", ""))
+        if isinstance(content, Content):
+            fields = {}
+            for field in content.field_dict.items():
+                representations = []
+                if len(field[1].get_external_index()) > 0:
+                    for representation in field[1].get_external_index():
+                        representations.append(representation)
+                    fields[field[0]] = representations
+
+            current_project.recommender_system.fields_representations[content_type] = fields
+            current_project.recommender_system.exogenous_techniques[content_type] = content.exogenous_rep_container.get_external_index()
+        else:
+            list_errors.append("Invalid items file in items directory <br>(<b>'" + path_to_content + "'</b>)")
+    except FileNotFoundError:
+        list_errors.append("<b>'" + path_to_content + "'</b> is not a valid directory.")
+    except OSError:
+        list_errors.append("Wrong syntax in path to content.")
+    return list_errors
+
+
+def get_fields_and_exogenous(content_type):
+    global current_project
+
+    fields = {field: [representation["id"] for representation in representations]
+              for field, representations in current_project.content_analyzer[content_type].fields_selected.items()
+              if len(representations) > 0}
+
+    exogenous = [technique['content'][0]['params'][1]['value']
+                 for technique in current_project.content_analyzer[content_type].exogenous_techniques
+                 if 'content' in technique and len(technique['content'][0]['params']) > 0]
+
+    current_project.recommender_system.fields_representations[content_type] = fields
+    current_project.recommender_system.exogenous_techniques[content_type] = exogenous
 
 
 @app.route("/recsys/representations", methods=['POST', 'GET'])
@@ -380,8 +504,15 @@ def recsys_representations():
             or current_project.recommender_system.get_page_status("Representations") == PossiblePageStatus.DISABLED:
         return redirect(url_for("index"))
 
+    if current_project.recommender_system.is_path_items_from_ca():
+        get_fields_and_exogenous("Items")
+
+    if current_project.recommender_system.is_path_users_from_ca():
+        get_fields_and_exogenous("Users")
+
     return render_template("./recsys/representations.html",
-                           fields_representations=current_project.recommender_system.content.field_dict,
+                           fields_representations=current_project.recommender_system.fields_representations,
+                           exogenous=current_project.recommender_system.exogenous_techniques,
                            project=current_project)
 
 
@@ -403,10 +534,10 @@ def execute_modules():
 
     if request.method == "POST":
         if request.json["module"] == "ContentAnalyzer":
+            content_type = request.json["contentType"]
             old_stderr = sys.stderr
             new_stderr = io.StringIO()
             sys.stderr = new_stderr
-
 
             log_capture_string = io.StringIO()
             ch = logging.StreamHandler(log_capture_string)
@@ -415,8 +546,8 @@ def execute_modules():
             ch.setFormatter(formatter)
             logger.addHandler(ch)
 
-            print(current_project.content_analyzer.produce_config_file())
-            script_run(current_project.content_analyzer.produce_config_file())
+            print(current_project.content_analyzer[content_type].produce_config_file())
+            script_run(current_project.content_analyzer[content_type].produce_config_file())
             log_com = log_capture_string.getvalue()
             log_capture_string.close()
 
@@ -437,7 +568,11 @@ def save_config_file():
         module = request.json["module"]
 
         try:
-            config_file = current_project.modules[module].produce_config_file()
+            if module == "ContentAnalyzer":
+                content_type = request.json["contentType"]
+                config_file = current_project.modules[module][content_type].produce_config_file()
+            else:
+                config_file = current_project.modules[module].produce_config_file()
 
             with open(current_project.save_path + "/" + current_project.name + "/content_analyzer_config.json", "w") as outfile:
                 json.dump(config_file, outfile, indent=4)
